1b3c2debdc3a9ed8df84875194bb4c696ea10933
A-Zahra <z.a.ahmadi18@gmail.com>
2020-01-22 13:55:02 -0500

    Ex1: Add new properties to pixels
    I added a new property to rotate function. When one of the right or left arrow keys is pressed all pixels get random colors of a same spectrum

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -1,66 +1,94 @@
 "use strict";
 
 /********************************************************************
-
-Title of Project
-Author Name
-
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
+Assignment 1
+Zahra Ahmadi
 
 *********************************************************************/
 
 window.onload = setup;
+
+// Variable which holds the value for rotation
 let rotation = 0;
+// Assemble all pixels in one array so that I can rotate them all to gether
 let allPixels = [];
+
+// setup()
+//
+// Sets up all new properties of html file elements
 function setup() {
-  console.log("hello, I am here!");
-  for(let i = 0; i < 1000; i++){
+  // A for loop which generates 1000 pixels
+  for (let i = 0; i < 1000; i++) {
+    // Calls the div element from Html file and assign it to a variable
     let pixel = document.createElement('div');
+    // Gives pixel variable a class name
     pixel.setAttribute('class', 'pixel');
+    // Add the new div called pixel to the body
     document.body.appendChild(pixel);
+    // Also adds it to the array
     allPixels[i] = pixel;
+    // Changes div color once mouse is over
     pixel.addEventListener('mouseover', paint);
+    // Set div opacity to 0 once mouse is clicked
     pixel.addEventListener('click', remove);
+    // Rotates div left or right once left or right arrow key is pressed
     document.addEventListener('keydown', rotate);
   }
 }
 
+// paint()
+// Sets random colors for the pixel
 function paint(e) {
   let pixel = e.target;
   let r = Math.random() * 255;
   let g = Math.random() * 10;
   let b = Math.random() * 10;
   pixel.style.backgroundColor = `rgb(${r},${g},${b})`;
-  // pixel.style.width = `${r}px`;
-  // pixel.style.height = `${r}px`;
+  // Resets pixel color to black after a second
   setTimeout(resetPixel, 1000, pixel);
 }
 
+// resetPixel()
+// Sets pixel color to black
 function resetPixel(pixel) {
   pixel.style.backgroundColor = 'black';
 }
 
+// remove()
+// Sets pixel opacity to Zero
 function remove(e) {
   let pixel = e.target;
-  pixel.style.opacity = '0';
-  console.log("worked!" + pixel);
+  let randomOpacity = 0;
+  pixel.style.opacity = randomOpacity;
 }
 
+// rotate()
+// rotates pixel to left or right once left or right arrow key is pressed
 function rotate(e) {
-  console.log(e.keyCode);
+  // Right rotation
   if (e.keyCode === 39) {
+    // Adds one to rotation degree
     rotation += 1;
     for (let i = 0; i < 1000; i++) {
-    //  console.log(allPixels[i]);
-      allPixels[i].style.transform = "rotate("+rotation+"deg)";
+      allPixels[i].style.transform = "rotate(" + rotation + "deg)";
+      // Assigns a new color to the pixel
+      let r = Math.random() * 10;
+      let g = Math.random() * 10;
+      let b = Math.random() * 255;
+      allPixels[i].style.backgroundColor = `rgb(${r},${g},${b})`;
     }
   }
+  // Left rotation
   else if (e.keyCode === 37) {
+    // Lowers rotation degree by 1 
     rotation += -1;
     for (let i = 0; i < 1000; i++) {
-    //  console.log(allPixels[i]);
-      allPixels[i].style.transform = "rotate("+rotation+"deg)";
+      allPixels[i].style.transform = "rotate(" + rotation + "deg)";
+      // Assigns a new color to the pixel
+      let r = Math.random() * 10;
+      let g = Math.random() * 255;
+      let b = Math.random() * 10;
+      allPixels[i].style.backgroundColor = `rgb(${r},${g},${b})`;
     }
   }
 }
