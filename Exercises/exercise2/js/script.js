3198d924ed5b45c2633e6c8c57c5a0eef7f10e74
A-Zahra <z.a.ahmadi18@gmail.com>
2020-01-23 16:01:49 -0500

    A2: Added starting code

---
+++ Exercises/exercise2/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/********************************************************************
+
+Title of Project
+Author Name
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+
+$(document).ready(setup);
+
+function setup() {
+  setInterval(update, 1000);
+  $('span').on("click", spanClicked);
+}
+
+function update() {
+  $('span').each(updateSpan);
+}
+
+function updateSpan() {
+  let randomNum = Math.random();
+  if (randomNum < 0.1) {
+    $(this).removeClass("redacted");
+    $(this).addClass("revealed");
+  }
+}
+
+function spanClicked() {
+  $(this).removeClass("revealed");
+  $(this).addClass("redacted");
+}
