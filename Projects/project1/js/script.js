fb61076e7c4889c0113f5530cd54d77c5d0845eb
A-Zahra <z.a.ahmadi18@gmail.com>
2020-01-31 20:36:34 -0500

    A1: Add images
    I added paper and trash bin images + increased number of paper trashes + constrained paper trashes movement to the area of my mainContainer div

--- Projects/project1/js/script.js
+++ Projects/project1/js/script.js
@@ -11,68 +11,85 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 
 $(document).ready(setup);
-let $bar;
-let $allBars = [];
-let bar;
-let allBars = [];
-let newBars = [];
-let barName;
+
+let $allPapers = [];
+let newpapers = [];
+const numPapers = 450;
+let paper;
+let paperName;
 let $trashBin;
 let randPosX;
 let randPosY;
+let randPosRight;
+
+// setup()
+//
+// Sets up everything
 function setup() {
-  for (let i=0; i < 10; i++) {
-    newBars[i] = `bar${i}`;
-    bar = document.createElement('div');
-    barName = newBars[i];
-    bar.setAttribute('class', barName);
-    console.log(bar);
-    document.getElementById('mainContainer').appendChild(bar);
-  }
-  
-  for (let i = 0; i < 10; i++){
+  // Create div elements + sets their attribute name +
+  // Append them to mainContainer div + Assign them to an array
+  for (let i=0; i < numPapers; i++) {
+    newpapers[i] = `paper${i}`;
+    paper = document.createElement('div');
+    paperName = newpapers[i];
+    paper.setAttribute('class', paperName);
+    document.getElementById('mainContainer').appendChild(paper);
+    }
+
+  //
+  for (let i = 0; i < numPapers; i++){
     let bodyWidth = $(window).width();
     let bodyHeight = $(window).height();
-    randPosX = Math.floor((Math.random()*(bodyWidth - 100)));
-    randPosY = Math.floor((Math.random()*(bodyHeight/1.3)));
-    $allBars[i] = $(`.bar${i}`);
-    console.log(randPosY);
-    let left = 10*i;
-    $allBars[i].css({"background-color": "yellow", "width": "100px", "height": "100px", "position": "fixed"});
-    $allBars[i].offset({ "top": randPosY, "left": randPosX});
-    $allBars[i].draggable({ containment: "parent" });
-  }
-  // $bar = $('.bar0');
-  // 
-  // $bar.offset({ top: randPosY, left: randPosX});
+    randPosX = Math.floor((Math.random()*(bodyWidth - 80)));
+    randPosRight = Math.floor((Math.random()*(bodyWidth / 40)));
+    randPosY = Math.floor((Math.random()*(bodyHeight / 1.1)));
 
-  // $bar.css( "background-color", "yellow" );
-    
-  // $bar.draggable();
+    $allPapers[i] = $(`.paper${i}`);
+    if (randPosX > 320 && randPosY < 500 && randPosY > 20){
+    $allPapers[i].offset({ "top": randPosY, "left": randPosX});
+}
+    let imageUrl = "../assets/images/wastepaper.png";
+    $allPapers[i].css({
+      "background-image": "url(" + imageUrl + ")",
+      "width": "100px",
+      "height": "100px",
+      "position": "absolute",
+      "background-position": "center center",
+      "background-repeat": "round",
+      "background-size": "cover",
+      "z-index": "0"
+    });
+
+    $allPapers[i].draggable({ containment: "parent"});
+    $allPapers[i].insertBefore( "#trashBin" );
+  }
+
+
   $trashBin = $('#trashBin');
   $trashBin.droppable({
       drop: onDrop,
     });
 
 }
-// 
+
+// onDrop()
+//
+// On drop removes the target paper
 function onDrop(event, ui) {
-  // let pos = ui.draggable.position();
-  //   alert('top: ' + pos.to+ ', left: ' + pos.left);
   console.log("dropped!");
-  for (let i = 0; i < 10; i++){
+  for (let i = 0; i < numPapers; i++){
     ui.draggable.remove();
   }
 }
 
 // let myWindow;
-// 
+//
 // function openWin() {
 //   myWindow = window.open("", "", "width=100, height=100");
 // }
-// 
+//
 // function resizeWin() {
 //   myWindow.resizeTo(250, 250);
 //   myWindow.focus();
 // }
-// 
+//
