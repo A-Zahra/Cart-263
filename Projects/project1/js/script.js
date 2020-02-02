8d47968f45882116774b7bea1c879a7b61911ac0
A-Zahra <z.a.ahmadi18@gmail.com>
2020-02-01 19:04:28 -0500

    A1: Add trash bin + timer + playbutton
    I added trash bin image and background + I made my code clearer by making seperate functions for different parts of my code + I added start screen and play button

--- Projects/project1/js/script.js
+++ Projects/project1/js/script.js
@@ -14,82 +14,132 @@ $(document).ready(setup);
 
 let $allPapers = [];
 let newpapers = [];
-const numPapers = 450;
+const numPapers = 600;
 let paper;
 let paperName;
 let $trashBin;
 let randPosX;
 let randPosY;
 let randPosRight;
+let seconds = 10;
+let intervalTime = 1000;
+let timeOver = true;
+let gameOverScreen;
+let startScreen = true;
+let $startMessage;
 
 // setup()
 //
 // Sets up everything
 function setup() {
+
+    makePapers();
+    assignPapersProperties();
+
+    $startMessage = $('#playButton');
+    $startMessage.one("click", startGame);
+
+    $trashBin = $('#trashBin');
+    $trashBin.droppable({
+      drop: onDrop,
+    });
+
+    $trashBin.hide();
+    $('#timer').hide();
+
+
+}
+
+function startGame() {
+  console.log("worked!");
+  timeOver = false;
+  $startMessage.hide();
+
+    for (let i=0; i < numPapers; i++){
+      $allPapers[i].show();
+    }
+      $trashBin.show();
+      $('#timer').show();
+      setInterval(updateTime, intervalTime);
+}
+
+function makePapers() {
   // Create div elements + sets their attribute name +
   // Append them to mainContainer div + Assign them to an array
-  for (let i=0; i < numPapers; i++) {
+  for (let i = 0; i < numPapers; i++) {
     newpapers[i] = `paper${i}`;
     paper = document.createElement('div');
     paperName = newpapers[i];
     paper.setAttribute('class', paperName);
     document.getElementById('mainContainer').appendChild(paper);
-    }
+  }
+}
 
-  //
-  for (let i = 0; i < numPapers; i++){
+function assignPapersProperties() {
+
+  for (let i = 0; i < numPapers; i++) {
     let bodyWidth = $(window).width();
     let bodyHeight = $(window).height();
-    randPosX = Math.floor((Math.random()*(bodyWidth - 80)));
-    randPosRight = Math.floor((Math.random()*(bodyWidth / 40)));
-    randPosY = Math.floor((Math.random()*(bodyHeight / 1.1)));
+    randPosX = Math.floor((Math.random() * (bodyWidth - 65)));
+    randPosRight = Math.floor((Math.random() * (bodyWidth / 40)));
+    randPosY = Math.floor((Math.random() * (bodyHeight)));
 
     $allPapers[i] = $(`.paper${i}`);
-    if (randPosX > 320 && randPosY < 500 && randPosY > 20){
-    $allPapers[i].offset({ "top": randPosY, "left": randPosX});
-}
+    if (randPosX > 730 && randPosY < 570 && randPosY > 100) {
+      $allPapers[i].offset({
+        "top": randPosY,
+        "left": randPosX
+      });
+    }
+
     let imageUrl = "../assets/images/wastepaper.png";
     $allPapers[i].css({
       "background-image": "url(" + imageUrl + ")",
-      "width": "100px",
-      "height": "100px",
+      "width": "70px",
+      "height": "70px",
       "position": "absolute",
       "background-position": "center center",
       "background-repeat": "round",
-      "background-size": "cover",
-      "z-index": "0"
+      "background-size": "cover"
     });
+    $allPapers[i].hide();
 
-    $allPapers[i].draggable({ containment: "parent"});
-    $allPapers[i].insertBefore( "#trashBin" );
-  }
-
-
-  $trashBin = $('#trashBin');
-  $trashBin.droppable({
-      drop: onDrop,
+    $allPapers[i].draggable({
+      containment: "parent"
     });
+    // $allPapers[i].insertBefore("#trashBin");
 
+    }
 }
+
+
 
 // onDrop()
 //
 // On drop removes the target paper
 function onDrop(event, ui) {
   console.log("dropped!");
-  for (let i = 0; i < numPapers; i++){
+  for (let i = 0; i < numPapers; i++) {
     ui.draggable.remove();
   }
 }
 
-// let myWindow;
-//
-// function openWin() {
-//   myWindow = window.open("", "", "width=100, height=100");
-// }
-//
-// function resizeWin() {
-//   myWindow.resizeTo(250, 250);
-//   myWindow.focus();
-// }
-//
+function updateTime() {
+  if (seconds > 0) {
+    seconds -= 1;
+    $('#seconds').text(seconds);
+  }
+  else {
+    $trashBin.hide();
+    $('#timer').hide();
+    for(let i = 0; i < numPapers; i++){
+      $allPapers[i].hide();
+    }
+
+    $startMessage.text('Play Again!');
+    $startMessage.show();
+    $startMessage.one("click", startGame);
+    seconds = 10;
+    intervalTime =1000;
+  }
+}
