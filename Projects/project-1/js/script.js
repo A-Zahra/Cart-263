a962f747093dace9cb5956826b9762dc4b1c258b
A-Zahra <z.a.ahmadi18@gmail.com>
2020-02-06 16:34:07 -0500

    A1: Add images to the code

--- Projects/project-1/js/script.js
+++ Projects/project-1/js/script.js
@@ -151,7 +151,7 @@ function assignPapersProperties() {
 
 
     // Sets paper properties
-    let imageUrl = "../assets/images/wastePaperImg.png";
+    let imageUrl = "../assets/images/waste-Paper.png";
     $allPapers[i].css({
       "background-image": "url(" + imageUrl + ")",
       "width": "70px",
