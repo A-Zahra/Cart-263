0c8421b2e84b876286af9058aa445a4396264d8d
A-Zahra <z.a.ahmadi18@gmail.com>
2020-02-16 22:36:06 -0500

    A3: Add "I think it is" command + add correct asnwer counter function
    I added a third command. The user can say the answer orally and it will be checked through the oralAnswer function to see if it is correct. If so it turns green. Otherwise, the button shakes. I also added a counter function which counts number of correct answers. If the answer is right, it is added to the total number.

--- Exercises/exercise3/js/script.js
+++ Exercises/exercise3/js/script.js
@@ -3,7 +3,7 @@
 /********************************************************************
 
 Title of Project
-Author Name
+Zahra Ahmadi
 
 This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
@@ -11,247 +11,279 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 
 $(document).ready(setup);
-let buttons; 
-const NUM_OPTIONS = 5;
-let $correctButton;
+let buttons; // Stores five random animal names
+const NUM_OPTIONS = 5; // Number of random animals being displayed on screen
+let $correctButton; // Button which was just said
+let numCorrectAnswer=0;
+// All animals names
 let animals = [
-      "aardvark",
-      "alligator",
-      "alpaca",
-      "antelope",
-      "ape",
-      "armadillo",
-      "baboon",
-      "badger",
-      "bat",
-      "bear",
-      "beaver",
-      "bison",
-      "boar",
-      "buffalo",
-      "bull",
-      "camel",
-      "canary",
-      "capybara",
-      "cat",
-      "chameleon",
-      "cheetah",
-      "chimpanzee",
-      "chinchilla",
-      "chipmunk",
-      "cougar",
-      "cow",
-      "coyote",
-      "crocodile",
-      "crow",
-      "deer",
-      "dingo",
-      "dog",
-      "donkey",
-      "dromedary",
-      "elephant",
-      "elk",
-      "ewe",
-      "ferret",
-      "finch",
-      "fish",
-      "fox",
-      "frog",
-      "gazelle",
-      "gila monster",
-      "giraffe",
-      "gnu",
-      "goat",
-      "gopher",
-      "gorilla",
-      "grizzly bear",
-      "ground hog",
-      "guinea pig",
-      "hamster",
-      "hedgehog",
-      "hippopotamus",
-      "hog",
-      "horse",
-      "hyena",
-      "ibex",
-      "iguana",
-      "impala",
-      "jackal",
-      "jaguar",
-      "kangaroo",
-      "koala",
-      "lamb",
-      "lemur",
-      "leopard",
-      "lion",
-      "lizard",
-      "llama",
-      "lynx",
-      "mandrill",
-      "marmoset",
-      "mink",
-      "mole",
-      "mongoose",
-      "monkey",
-      "moose",
-      "mountain goat",
-      "mouse",
-      "mule",
-      "muskrat",
-      "mustang",
-      "mynah bird",
-      "newt",
-      "ocelot",
-      "opossum",
-      "orangutan",
-      "oryx",
-      "otter",
-      "ox",
-      "panda",
-      "panther",
-      "parakeet",
-      "parrot",
-      "pig",
-      "platypus",
-      "polar bear",
-      "porcupine",
-      "porpoise",
-      "prairie dog",
-      "puma",
-      "rabbit",
-      "raccoon",
-      "ram",
-      "rat",
-      "reindeer",
-      "reptile",
-      "rhinoceros",
-      "salamander",
-      "seal",
-      "sheep",
-      "shrew",
-      "silver fox",
-      "skunk",
-      "sloth",
-      "snake",
-      "squirrel",
-      "tapir",
-      "tiger",
-      "toad",
-      "turtle",
-      "walrus",
-      "warthog",
-      "weasel",
-      "whale",
-      "wildcat",
-      "wolf",
-      "wolverine",
-      "wombat",
-      "woodchuck",
-      "yak",
-      "zebra"
-    ];
-let answer = 0;
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra"
+];
+
 function setup() {
+  // On click, runs the game
   $(document).one('click', newRound);
   
-      
-    if (annyang) {
+  // Speech recognition code which gives user the opportunity to orally interact with the website
+  if (annyang) {
     // Let's define our first command. First the text we expect, and then the function it should call
     let commands = {
-      'I give up': giveUp,
-      'Say it again': repeatReversedWord,
-      'I think it is ()': oralAnswer
+      'I give up': giveUp, // If user said "I give up", prompts giveUp function
+      'Say it again': repeatReversedWord, // If user said "Say it again", repeats the reversed word
+      'I think it is *text': oralAnswer 
     };
     // Add our commands to annyang
     annyang.addCommands(commands);
 
     // Start listening. You can call this here, or attach this call to an event, button, etc.
     annyang.start();
+    
+    // Activates debug mode for detailed logging in the console
+    // annyang.debug();
   }
 }
-function addButton(label){
+
+// Creates buttons, assigns css properties to
+function addButton(label) {
   let $button = $('<div></div>').addClass("guess").text(label);
   $button.button();
   $button.css({
     "position": "relative",
     "margin": "auto",
     "width": "auto",
-    "height":"auto",
+    "height": "auto",
   });
+  // On click, prompts handleGuess function
   $button.on('click', handleGuess);
   return $button;
 }
 
+// Gets random animal names from the array
 function getRandomElement(array) {
   let element = array[Math.floor(Math.random() * array.length)];
   return element;
 }
 
-function newRound() { 
-  let mainContainer = $('<div></div>').addClass("mainContainer");
+// Defines a main container div + assigns properties to 
+function newRound() {
+  let mainContainer = $('<div></div>').attr('id', 'mainContainer');
   mainContainer.appendTo('body');
-  mainContainer.css({
-    "width": "50%",
-    "height":"10%",
-    "background-color": "yellow",
-    "display": "inline-flex",
-    "position": "absolute",
-    "margin": "auto",
-    "margin-top": "5vw",
-    "top": "0",
-    "right": "0",
-    "bottom": "0",
-    "left": "0",
-    "padding": "3vw"
-  });
+  
+  correctAnswerCounter();
   
   buttons = [];
-  for(let i = 0; i < NUM_OPTIONS; i++){
+  for (let i = 0; i < NUM_OPTIONS; i++) {
+    // Assigns five random animal names to buttons array
     let $button = addButton(getRandomElement(animals));
     buttons.push($button);
+    // Appends buttons content to main container
     buttons[i].appendTo(mainContainer);
   }
-
+  // Takes a random animal name from buttons array and assigns it to correctButton variable
   $correctButton = getRandomElement(buttons);
+  // Sends it through the sayBackwards function
   sayBackwards($correctButton.text());
 
 }
 
+// Displays and Counts number of correct answers
+function correctAnswerCounter () {
+  let correctAnswer = $('#correctAnswer');
+  correctAnswer.text(`Number of correct answers:${numCorrectAnswer}`);
+}
+
+// On click, if the value of button clicked was same as the value of correctbutton..
 function handleGuess() {
   if ($(this).text() === $correctButton.text()) {
+    // Removes all the divs named "guess"
     $(".guess").remove();
+    // Makes a new round of buttons
     setTimeout(newRound, 100);
-  }
-  else {
+    numCorrectAnswer++;
+
+  } else {
+    // Otherwise, applies shake effect to the button
     $(this).effect('shake');
+    // Repeats the word backward
     sayBackwards($correctButton.text());
+    numCorrectAnswer = 0;
   }
 }
 
-function sayBackwards(text){
+// In new round say the value of correctButton backward
+function sayBackwards(text) {
+  // Reverses the word
   let backwardsText = text.split('').reverse().join('');
   let options = {
     rate: Math.random(),
     pitch: Math.random()
   };
+  // Says it
   responsiveVoice.speak(backwardsText, "UK English Male", options);
 }
 
-function giveUp(){
+// If giveUp function got called:
+function giveUp() {
+  // Changes correctButton background and text color
+  $correctButton.css({
+    "background-color": "green",
+    "color": "white"
+  });
+}
+
+// If repeatReversedWord got called say the word again
+function repeatReversedWord() {
+  sayBackwards($correctButton.text());
+}
+
+// 
+function oralAnswer(myString) {
+  if ( myString === $correctButton.text()) {
+    // Changes correctButton background and text color
     $correctButton.css({
       "background-color": "green",
-      "color":"white"
+      "color": "white"
     });
-}
-
-function repeatReversedWord() {
-    sayBackwards($correctButton.text());
-}
-
-function oralAnswer() {
-  if (answer === $correctButton.text() ) {
-    $('body').css({
-      "background-color" : "orange"
-    })
+  }
+  else {
+      // Otherwise, applies shake effect to the button
+      $(this).effect('shake');
+      // Repeats the word backward
+      sayBackwards($correctButton.text());
+      numCorrectAnswer = 0;
+    
   }
 }
