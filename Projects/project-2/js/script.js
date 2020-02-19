fce35a068a38e76b5425ab09e59209bb5e6c2f84
A-Zahra <z.a.ahmadi18@gmail.com>
2020-02-18 19:31:53 -0500

    P2: Add starting code
    I am borrowing assignment 3 files

---
+++ Projects/project-2/js/script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+/*****************
+
+Guess who i am card game
+Zahra Ahmadi
+
+
+
+Reference:
+
+ResponsiveVoice
+https://responsivevoice.org/
+
+Animal names from:
+
+******************/
+
+// An array of animal names that we use to create our guessing game
+let animals = [
+  "baboon",
+  "bat",
+  "bear",
+  "buffalo",
+  "camel",
+  "cat",
+  "cheetah",
+  "cow",
+  "crocodile",
+  "crow",
+  "deer",
+  "dog",
+  "donkey",
+  "fish",
+  "fox",
+  "frog",
+  "giraffe",
+  "goat",
+  "gorilla",
+  "grizzly bear",
+  "hamster",
+  "hippopotamus",
+  "horse",
+  "kangaroo",
+  "koala",
+  "leopard",
+  "lion",
+  "llama",
+  "monkey",
+  "mouse",
+  "panda",
+  "panther",
+  "parrot",
+  "pig",
+  "polar bear",
+  "rabbit",
+  "raccoon",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "seal",
+  "sheep",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tiger",
+  "turtle",
+  "whale",
+  "wolf",
+  "zebra"
+];
+
+// Get setup!
+$(document).ready(setup);
+
+// setup()
+//
+// 
+function setup() {
+
+}
+
