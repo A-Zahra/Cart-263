7ee998b0380a9253e3d4ee76d4ee7cd7db358eef
A-Zahra <z.a.ahmadi18@gmail.com>
2020-02-20 21:10:32 -0500

    P2: Add images + give randomness to cards display + add score counter
    I am adding more animal images. I made a function which randomly picks the cards every time the page is loaded. I also added a score counter which is going to counts number of correct answers.

--- Projects/project-2/js/script.js
+++ Projects/project-2/js/script.js
@@ -7,6 +7,37 @@ Zahra Ahmadi
 
 
 
+"buffalo",
+"camel",
+"cat",
+"cow",
+"crocodile",
+"crow",
+"hippopotamus",
+"horse",
+"kangaroo",
+"koala",
+"lion",
+"donkey",
+"goat",
+"fish",
+"monkey",
+"mouse",
+"panda",
+"pig",
+"rabbit",
+"raccoon",
+"rhinoceros",
+"seal",
+"sheep",
+
+"snake",
+"squirrel",
+"tiger",
+"turtle",
+"whale",
+"wolf",
+"zebra"
 Reference:
 
 ResponsiveVoice
@@ -20,72 +51,40 @@ Animal names from:
 let animals = [
   "dog",
   "deer",
-  "donkey",
-  "fish",
-  "fox",
-  "frog",
-  "giraffe",
-  "goat",
   "gorilla",
-  "baboon",
-  "bat",
-  "bear",
-  "buffalo",
-  "camel",
-  "cat",
-  "cow",
-  "crocodile",
-  "crow",
-  "hippopotamus",
-  "horse",
-  "kangaroo",
-  "koala",
-  "leopard",
-  "lion",
-  "monkey",
-  "mouse",
-  "panda",
-  "parrot",
-  "pig",
-  "rabbit",
-  "raccoon",
-  "rhinoceros",
-  "seal",
-  "sheep",
+  "frog",
   "sloth",
-  "snake",
-  "squirrel",
-  "tiger",
-  "turtle",
-  "whale",
-  "wolf",
-  "zebra"
+  "bear",
+  "jaguar",
+  "parrot"
 ];
 let questionList = [
-  "I have big white teeth and I love carrot!",
-    "I have mane around my neck and I am the king of jungle!",
-  "sloth",
-  "snake",
-  "squirrel",
-  "tiger",
-  "turtle",
-  "whale",
-  "wolf"
+  "I am playful and do not share my lovely bone with anyone!",
+  "I have big antlers and a fluffy tail! I live both in north and south America!",
+  "I love banana! I am black and have large jaw muscles plus strong teeth!",
+  "I live on wetLands and rivers. Flies are my favorite food!",
+  "I am sooo Slow and love hanging trees!",
+  "I love honey and fish! I have rounded ears and big claws!",
+  "I am a wild cat that his fur is marked with rosettes!",
+  "I like to repeat people words and I have colorful wings!"
 ];
 let correctAnswer;
 let selfDefinition;
 let whoAmI;
 let cardImg;
 let cardNum = 0;
+let numPointBars = 1;
+let pointBarTopMargin = 0;
 // Get setup!
 $(document).ready(setup);
 
 // setup()
 //
-// 
+//
 function setup() {
   $(".column1").one('click', cardInfo);
-  
+
+
   // Speech recognition code which gives user the opportunity to orally interact with the website
   if (annyang) {
     // Let's define our first command. First the text we expect, and then the function it should call
@@ -101,14 +100,28 @@ function setup() {
     // Activates debug mode for detailed logging in the console
     // annyang.debug();
   }
-  
 }
 
+
 function nextCard() {
-  cardNum++;
   cardInfo();
   cardImg.css({"display":"none"});
-  
+
+
+}
+
+function getRandomElement(animal){
+  let cardNumber;
+  let element = animal[Math.floor(Math.random() * animal.length)];
+  console.log("element "+ element);
+  for (let i = 0; i < animal.length; i++){
+    if (element === animal[i]){
+     cardNumber = i;
+      break;
+    }
+  }
+  // console.log("cardnumber: " + cardNumber);
+  return cardNumber;
 }
 
 function cardInfo() {
@@ -117,42 +130,35 @@ function cardInfo() {
   whoAmI.fadeIn(1000);
   whoAmI.css({"display":"table"}).after($("#selfDefinition"));
   questions();
+    pointCounter();
 }
 
 function questions() {
-  let r = Math.floor(Math.random(0,1)*10);
-  console.log(r);
+  if (animals.length !== 0){
+      cardNum  = getRandomElement(animals);
+  }
+  else if (animals.length < 0){
+    cardNum = undefined;
+    console.log("finished!");
+  }
+  // let r = Math.floor(Math.random(0,1)*10);
+  // console.log(r);
   selfDefinition = $("#selfDefinition");
   let definition = questionList[cardNum];
   selfDefinition.text(definition);
   selfDefinition.fadeIn(1000);
   selfDefinition.css({"display":"table"});
   correctAnswer = animals[cardNum];
-
-  
   // selfDefinition.bind('click', showMe);
 }
 
-function showMe(){
-  console.log("worked");
-  // if (answer === "dog") {
-    // let trueAnswer = $('<div></div>').attr('id', 'trueAnswer').text("Yes the answer is " + animals[0]);
-    // trueAnswer.css({
-    //   "font-family":"sans-serif",
-    //   "font-size":"2vw",
-    //   "display":"table",
-    //   "width": "50%",
-    //   "position": "relative",
-    //   "margin": "auto",
-    //   "text-align": "center",
-    //   "display": "table"
-    // });
-    
+function showMe(myString){
+  if (myString === animals[cardNum]) {
     cardImg = $('img').addClass(animals[cardNum]);
-    console.log(animals[cardNum]);
+    console.log("animal name: "+animals[cardNum]);
     $(`.${animals[cardNum]}`).attr('src', 'assets/images/' + animals[cardNum] + ".png");
     cardImg.css({
-        "display":"none",  
+        "display":"none",
         "width": "100%",
         "height":"100%",
         "position": "relative",
@@ -160,13 +166,59 @@ function showMe(){
         "top": "0",
         "bottom": "0",
         "right": "0",
-        "left": "0"
+        "left": "0",
+        "float":"left"
     });
-    // trueAnswer.appendTo(".column1");
+
     cardImg.fadeIn(500);
     selfDefinition.css({"display":"none"});
     whoAmI.css({"display":"none"});
-  // }
+    // }
+    questionList = jQuery.grep(questionList, function(value) {
+    return value != questionList[cardNum];
+    });
+    console.log(questionList);
+    animals = jQuery.grep(animals, function(value) {
+    return value != animals[cardNum];
+    });
+    numPointBars++;
+    pointBarTopMargin += 1;
+  }
 
+  console.log(animals);
 
+    // console.log("worked");
+    // if (answer === "dog") {
+      // let trueAnswer = $('<div></div>').attr('id', 'trueAnswer').text("Yes the answer is " + animals[0]);
+      // trueAnswer.css({
+      //   "font-family":"sans-serif",
+      //   "font-size":"2vw",
+      //   "display":"table",
+      //   "width": "50%",
+      //   "position": "relative",
+      //   "margin": "auto",
+      //   "text-align": "center",
+      //   "display": "table"
+      // });
+
+    // trueAnswer.appendTo(".column1");
+
+}
+
+function pointCounter() {
+  let counter = $('<div></div>').attr('id', 'counter');
+
+  console.log();
+  let pointBars = [];
+  for (let i = 0; i < numPointBars ; i++) {
+    counter.css({
+      "margin-top": `${pointBarTopMargin}vw`,
+      "opacity": `${numPointBars/10}`
+    });
+    pointBars.push(counter);
+    pointBars[i].appendTo("#pointCounter");
+
+    console.log("worked");
+
+  }
 }
