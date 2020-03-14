877a946e3fb38a0f5cbf6d97a47cbf7122e182cb
A-Zahra <z.a.ahmadi18@gmail.com>
2020-03-13 20:12:32 -0400

    A4: Add start screen
    I am adding an start screen to give the user an idea of what they should expect to see or read.

--- Exercises/exercise4/js/script.js
+++ Exercises/exercise4/js/script.js
@@ -10,15 +10,20 @@ to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
 let condimentSimileSentence;
+let gameStarted= false;
 $(document).ready(setup);
 
 // setup()
 //
 // Prompts all the required functons to run the code
 function setup() {
-  $.getJSON("data/data.json")
-    .done(dataLoaded) // If there is no error, runs dataLoaded function
-    .fail(dataError); // otherwise, runs the dataError function
+  let introduction = $('<h2></h2>').text("Let's see what monsters eat as their daily meal!").attr('id', 'introduction').appendTo('body');
+  let monsterImg = $('<img>').addClass("monsterImg").attr('src', 'assets/images/lovelyMonster.png').appendTo('body');
+  if (gameStarted) {
+    $.getJSON("data/data.json")
+      .done(dataLoaded) // If there is no error, runs dataLoaded function
+      .fail(dataError); // otherwise, runs the dataError function
+  }
 }
 
 // dataLoaded()
@@ -68,11 +73,16 @@ function dataLoaded(data) {
     }
   }
 
+  //Gets random object's name from JSON file
+  let randomObject = getRandomElement(data.objects);
+  // Gets random appliance's name from JSON file
+  let randomAppliances = getRandomElement(data.appliances);
+  
   // Creates a container div
   let container = $('<div></div>').attr('id', 'container');
   container.appendTo('body');
   // Asigns Condiment Simile to a variable
-  let condimentSimile = `${randomCondiment} ${verb} like ${catArticle} ${randomCats} in ${roomArticle} ${randomRoom}.`;
+  let condimentSimile = `${randomCondiment} ${verb} like ${catArticle} ${randomCats} in ${roomArticle} ${randomRoom} that when you add it to your ${randomAppliances} it tastes like a ${randomObject}`;
   condimentSimileSentence = $('<p></p>').text(`${condimentSimile}`).attr('id', 'condimentSimile');
   // Append it to the container div
   condimentSimileSentence.appendTo('#container');
